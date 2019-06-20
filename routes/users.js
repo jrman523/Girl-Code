a0adbdca43528334e710def3c58aa87f63e8f0bd
Maria Camila Restrepo <m.restrepo95@gmail.com>
2019-06-20 11:02:18 -0400

    fixed register bug

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/login', (req, res) => res.render('login'));
 router.get('/register', (req, res) => res.render('register'));
 
 //Register Handle
-router.post('./register', (req, res) => {
+router.post('/register', (req, res) => {
     console.log(req.body)
     res.send('hello');
 });

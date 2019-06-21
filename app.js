557ec37961247d3a5f898100cba313521f9fecb7
Maria Camila Restrepo <m.restrepo95@gmail.com>
2019-06-20 20:27:01 -0400

    fixed loading bugs

--- app.js
+++ app.js
@@ -1,58 +1,62 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
-const passport = require('passport');
 
 const app = express();
 
-//Passport Config
+// Passport Config
 require('./config/passport')(passport);
 
-//DB Config
-const db = require('./config/keys').MongoURI;
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-//Conect to Mongo
-mongoose.connect(db, { useNewUrlParser: true})
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-//Bodyparser
-app.use(express.urlencoded({  extended: false }));
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
-//Express Session
-app.use(session({
+// Express session
+app.use(
+  session({
     secret: 'secret',
     resave: true,
-    saveUninitialized: true,
-  }));
+    saveUninitialized: true
+  })
+);
 
-
-//Passport Middleware
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Connect Flash
+// Connect flash
 app.use(flash());
 
-//Global Vars
-app.use((req, res, next) => {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
 
-//Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,6 @@
+dbPassword = 'mongodb+srv://MariaCRestrepo:'+ encodeURIComponent('Make80save15!') + '@girlcode-qsiqe.mongodb.net/test?retryWrites=true';
+            // 'mongodb+srv://MariaCRestrepo:<password>@girlcode-qsiqe.mongodb.net/test?retryWrites=true&w=majority';
+
 module.exports = {
-    MongoURI: 'mongodb+srv://MariaCRestrepo:<Make80save15!>@girlcode-qsiqe.mongodb.net/test?retryWrites=true&w=majority'
-}
+    mongoURI: dbPassword
+};

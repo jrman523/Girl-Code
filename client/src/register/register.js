c11f4f64e7415f1bacb2c918a6661fbc4018c13f
jad <jadelramadi@gmail.com>
2019-07-01 23:31:48 -0400

    added a redirect to login

--- client/src/register/register.js
+++ client/src/register/register.js
@@ -9,14 +9,15 @@ const card = {
 }
 
 class Register extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
             name: '',
             email: '',
             password: '',
-            password2: ''
+            password2: '',
+            redirectTo: null
         }
 
         this.handleNameChange = this.handleNameChange.bind(this);
@@ -26,54 +27,54 @@ class Register extends React.Component {
         this.handleRegister = this.handleRegister.bind(this);
     }
 
-    handleNameChange(e){
+    handleNameChange(e) {
         this.setState({
-            name : e.target.value
+            name: e.target.value
         });
     }
 
-    handleEmailChange(e){
+    handleEmailChange(e) {
         this.setState({
-            email : e.target.value
+            email: e.target.value
         });
     }
 
-    handlePasswordChange(e){
+    handlePasswordChange(e) {
         this.setState({
-            password : e.target.value
+            password: e.target.value
         });
     }
 
-    handlePassword2Change(e){
+    handlePassword2Change(e) {
         this.setState({
-            password2 : e.target.value
+            password2: e.target.value
         });
     }
 
-    handleRegister(e){
+    handleRegister(e) {
 
-        if(this.state.password === this.state.password2){
+        if (this.state.password === this.state.password2) {
             axios.post('/user/', {
                 name: this.state.name,
                 email: this.state.email,
-                password : this.state.passwor
+                password: this.state.password
             }).then(res => {
-                if (res.data){
+                if (res.data) {
                     console.log('you have are signed up');
                     this.setState({ //redirect to login page
-						redirectTo: '/login'
-					})
-                }else {
+                        redirectTo: '/login'
+                    })
+                } else {
                     console.log('we had an error');
                 }
             }).catch(err => {
                 console.log('register server error: ');
                 console.log(err);
             });
-        }else {
+        } else {
             console.log('passwords do not match');
         }
-      
+
         e.preventDefault();
 
         this.setState({
@@ -85,39 +86,43 @@ class Register extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <title>Register</title>
-                <div className="row mt-5">
-                    <div className="col-md-6 m-auto">
-                        <div className="card card-body" style={card}>
-                            <h1 className="text-center mb-3"> Register</h1>
-                            <form action="/users/register" method="POST">
-                                <div className="form-group">
-                                    <label htmlFor="name">Name</label>
-                                    <input type="text" id="name" name="name" className="form-control" placeholder="Enter Name" value = {this.state.name} onChange={this.handleNameChange}/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" id="email" name="email" className="form-control" placeholder="Enter Email" value = {this.state.email} onChange={this.handleEmailChange}/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password" id="password" name="password" className="form-control" placeholder="Create Password" value = {this.state.password} onChange={this.handlePasswordChange}/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password2">Confirm Password</label>
-                                    <input type="password" id="password2" name="password2" className="form-control" placeholder="Confirm Password" value = {this.state.password2} onChange={this.handlePassword2Change}/>
-                                </div>
-                                <button className="btn btn-primary btn-block black" onClick={this.handleRegister}>Register</button>
-                            </form>
-                            <p className="lead mt-4">Have An Account? <Link to="/login">Login</Link></p>
+        if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        } else {
+            return (
+                <div>
+                    <title>Register</title>
+                    <div className="row mt-5">
+                        <div className="col-md-6 m-auto">
+                            <div className="card card-body" style={card}>
+                                <h1 className="text-center mb-3"> Register</h1>
+                                <form action="/users/register" method="POST">
+                                    <div className="form-group">
+                                        <label htmlFor="name">Name</label>
+                                        <input type="text" id="name" name="name" className="form-control" placeholder="Enter Name" value={this.state.name} onChange={this.handleNameChange} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input type="email" id="email" name="email" className="form-control" placeholder="Enter Email" value={this.state.email} onChange={this.handleEmailChange} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password" id="password" name="password" className="form-control" placeholder="Create Password" value={this.state.password} onChange={this.handlePasswordChange} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password2">Confirm Password</label>
+                                        <input type="password" id="password2" name="password2" className="form-control" placeholder="Confirm Password" value={this.state.password2} onChange={this.handlePassword2Change} />
+                                    </div>
+                                    <button className="btn btn-primary btn-block black" onClick={this.handleRegister}>Register</button>
+                                </form>
+                                <p className="lead mt-4">Have An Account? <Link to="/login">Login</Link></p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </div >
-        );
+                </div >
+            );
+        }
     }
 };
 export default Register;

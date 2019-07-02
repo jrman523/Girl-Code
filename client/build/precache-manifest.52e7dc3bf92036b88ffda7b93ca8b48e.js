792f12211b625f8faddfd04de8d08bac0f6daab8
jad <jadelramadi@gmail.com>
2019-07-02 11:11:42 -0400

    added build server

---
+++ client/build/precache-manifest.52e7dc3bf92036b88ffda7b93ca8b48e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669879022dac436f27ac1b713d589773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3f134f07eed5dfc8aac",
+    "url": "/static/css/main.cef03b71.chunk.css"
+  },
+  {
+    "revision": "251dddb6b31fae8fea68",
+    "url": "/static/js/2.02de1936.chunk.js"
+  },
+  {
+    "revision": "d3f134f07eed5dfc8aac",
+    "url": "/static/js/main.2993b65b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

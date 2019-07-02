7dcf115447544a31938ca18286053e0d1fe08d42
jad <jadelramadi@gmail.com>
2019-07-01 23:34:59 -0400

    added a redirect

--- client/src/login/login.js
+++ client/src/login/login.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom';
 import './style.css';
-import { Link } from "react-router-dom";
 import axios from 'axios';
 
 const card = {
@@ -18,7 +17,7 @@ class Login extends React.Component {
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
-  
+
     };
 
     handleChange(event) {
@@ -32,9 +31,9 @@ class Login extends React.Component {
         console.log('handleSubmit')
 
         axios.post('/user/login', {
-                email: this.state.email,
-                password: this.state.password
-            })
+            email: this.state.email,
+            password: this.state.password
+        })
             .then(response => {
                 console.log('login response: ')
                 console.log(response)
@@ -52,41 +51,41 @@ class Login extends React.Component {
             }).catch(error => {
                 console.log('login error: ')
                 console.log(error);
-                
+
             })
     }
 
-   render(){
-    if(this.state.redirectTo){
-        return <Redirect to={{ pathname: this.state.redirectTo }} />
-    }else{
-        return (
-            <div>
-                <title>Login</title>
-                <div className="row mt-5">
-                    <div className="col-md-6 m-auto">
-                        <div className="card card-body" style={card}>
-                            <h1 className="text-center mb-3"><i className="fas fa-sign-in-alt"></i>  Login</h1>
-                            <form action="/users/login" method="POST">
-                                <div className="form-group">
-                                    <label htmlFor="email">Email</label>
-                                    <input type="email" id="email" name="email" className="form-control" placeholder="Enter Email" value={this.state.username} onChange={this.handleChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password</label>
-                                    <input type="password" id="password" name="password" className="form-control" placeholder="Enter Password" value={this.state.password} onChange={this.handleChange} />
-                                </div>
-                                <button className="btn btn-primary btn-block black" onClick={this.handleSubmit} >Login</button>
-                            </form>
-                            <p className="lead mt-4">
-                                No Account? <Link to="/register">Register</Link>
-                            </p>
+    render() {
+        if (this.state.redirectTo) {
+            return <Redirect to={{ pathname: this.state.redirectTo }} />
+        } else {
+            return (
+                <div>
+                    <title>Login</title>
+                    <div className="row mt-5">
+                        <div className="col-md-6 m-auto">
+                            <div className="card card-body" style={card}>
+                                <h1 className="text-center mb-3"><i className="fas fa-sign-in-alt"></i>  Login</h1>
+                                <form action="/users/login" method="POST">
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email</label>
+                                        <input type="email" id="email" name="email" className="form-control" placeholder="Enter Email" value={this.state.username} onChange={this.handleChange} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password" id="password" name="password" className="form-control" placeholder="Enter Password" value={this.state.password} onChange={this.handleChange} />
+                                    </div>
+                                    <button className="btn btn-primary btn-block black" onClick={this.handleSubmit} >Login</button>
+                                </form>
+                                <p className="lead mt-4">
+                                    No Account? <Link to="/register">Register</Link>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
-   }
 };
 export default Login;
--- client/src/register/register.js
+++ client/src/register/register.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import axios from 'axios';
 
 const card = {
